257e7754cab65c045fe2c67358b85ff7c16b3107
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-04-29 22:30:30 -0300

    Building the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => (
+  <main className="relative">
+    nav
+    <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
+    <section className="padding">Popular products</section>
+    <section className="padding">Super quality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding-x py-10">Special offers</section>
+    <section className="bg-pale-blue padding">Customer reviews</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+  </main>
+);
+
+export default App;
