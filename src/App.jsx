c35303650d8cb1198de06bf8e6cbe65e5438d570
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-04-29 22:42:23 -0300

    Creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  SpecialOffers,
+  CustomerReviews,
+  Services,
+  Subscribe,
+  Footer,
+} from "./sections/index";
+
 const App = () => (
   <main className="relative">
     nav
-    <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
-    <section className="padding">Popular products</section>
-    <section className="padding">Super quality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding-x py-10">Special offers</section>
-    <section className="bg-pale-blue padding">Customer reviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding-x py-10">
+      <SpecialOffers />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
   </main>
 );
 
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import SpecialOffers from "./SpecialOffers";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  SpecialOffers,
+  CustomerReviews,
+  Services,
+  Subscribe,
+  Footer,
+};
